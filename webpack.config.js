11b89c7847bf98924b746b0edc2baad9bdc7bcfa
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-09-17 21:33:48 +0300

    Сделал HTML-скелет сайта

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -15,6 +16,19 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "public", "index.html"),
             title: "Layout and prototyping"
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "public"),
+                    to: path.resolve(__dirname, "dist"),
+                    filter: resourcePath=> !resourcePath.endsWith("index.html")
+                },
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: path.resolve(__dirname, "dist", "assets"),
+                }
+            ]
         }),
         new CleanWebpackPlugin()
     ],
