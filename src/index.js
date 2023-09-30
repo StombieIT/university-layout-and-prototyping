9256761861a52cd3d1b5652642f733f1741962eb
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-09-30 17:13:26 +0300

    Сверстал карусель и верхнюю часть сайта

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import "./index.scss";
 import "./image.scss";
+
 import notificationsIcon from "@assets/notifications.svg";
+import starIcon from "@assets/star.svg";
+import playIcon from "@assets/play.svg";
+import arrowLeftIcon from "@assets/arrow_left.svg";
+import arrowRightIcon from "@assets/arrow_right.svg";
 
 import { insertAllByClass } from "@utils/html";
 
-insertAllByClass("__notifications", notificationsIcon);
+insertAllByClass("js-notifications", notificationsIcon);
+insertAllByClass("js-star", starIcon);
+insertAllByClass("js-play", playIcon);
+insertAllByClass("js-arrow-left", arrowLeftIcon);
+insertAllByClass("js-arrow-right", arrowRightIcon);
