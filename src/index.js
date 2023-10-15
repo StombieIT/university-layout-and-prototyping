0c8805e99b36641681189ef58e98534a3cfa2220
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-10-15 14:41:09 +0300

    Сделал вёрстку

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import starIcon from "@assets/star.svg";
 import playIcon from "@assets/play.svg";
 import arrowLeftIcon from "@assets/arrow_left.svg";
 import arrowRightIcon from "@assets/arrow_right.svg";
+import moreIcon from "@assets/more.svg";
+import addIcon from "@assets/add.svg";
+import sendIcon from "@assets/send.svg";
+import instagramIcon from "@assets/instagram.svg";
+import facebookIcon from "@assets/facebook.svg";
+import vkIcon from "@assets/vk.svg";
 
 import { insertAllByClass } from "@utils/html";
 
@@ -14,3 +20,9 @@ insertAllByClass("js-star", starIcon);
 insertAllByClass("js-play", playIcon);
 insertAllByClass("js-arrow-left", arrowLeftIcon);
 insertAllByClass("js-arrow-right", arrowRightIcon);
+insertAllByClass("js-more", moreIcon);
+insertAllByClass("js-add", addIcon);
+insertAllByClass("js-send", sendIcon);
+insertAllByClass("js-instagram", instagramIcon);
+insertAllByClass("js-facebook", facebookIcon);
+insertAllByClass("js-vk", vkIcon);
