5fada7336c4681d3d25a7a51344dcd278bd853dd
Vladislav Yartsev <vladislav.yartsev@vk.team>
2023-10-16 23:51:48 +0300

    Исправил загрузку стилей

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
@@ -48,6 +49,7 @@ module.exports = {
                 }
             ]
         }),
+        new MiniCssExtractPlugin(),
         new CleanWebpackPlugin()
     ],
     module: {
@@ -61,14 +63,14 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader'
                 ]
             },
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
                     'sass-loader'
                 ]
